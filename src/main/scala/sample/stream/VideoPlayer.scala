83161c66174132475766e8b1253a1e76e0936b79
Josh Suereth <joshua.suereth@gmail.com>
2014-06-26 13:14:01 -0400

    Add example of adapting a stream of control events into a play-pause stream.

---
+++ src/main/scala/sample/stream/VideoPlayer.scala
@@ -0,0 +1,140 @@
+package sample.stream
+
+import scala.concurrent.duration._
+import akka.actor.ActorSystem
+import akka.stream.FlowMaterializer
+import akka.stream.MaterializerSettings
+import akka.stream.scaladsl.Flow
+import akka.util.Timeout
+import java.io.File
+import org.reactivestreams.api.Producer
+import video.Frame
+import org.reactivestreams.api.Processor
+import video.UIControl
+import akka.actor.Actor
+import org.reactivestreams.spi.Subscriber
+import akka.actor.Props
+import org.reactivestreams.spi.Subscription
+import org.reactivestreams.spi.Publisher
+
+object VideoPlayer {
+
+  /**
+   * Use parameters `server 0.0.0.0 6001` to start server listening on port 6001.
+   *
+   */
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("test")
+    val settings = MaterializerSettings()
+    val materializer = FlowMaterializer(settings)
+    implicit val timeout = Timeout(5.seconds)
+    var count = 0L
+    val (player, uiControls) = video.Display.createPlayer
+    val playerProcessor = new PlayerProcessor(system, new File(args(0)))
+    uiControls produceTo playerProcessor
+    playerProcessor produceTo player
+    ()
+  }
+}
+
+// An implementation of a processor.  takes a UI Control input stream
+// and 'controls' an underlying file input stream, ensuring that playing happens only when in
+// the play state.
+class PlayerProcessor(system: ActorSystem, file: File) extends video.AbstractProducer[Frame] with Processor[UIControl, Frame] {
+  private val playerActor = system.actorOf(Props(new PlayerActor))
+  private class PlayerActor extends Actor {
+    private var subscribers: Set[Sub] = Set.empty
+    private var videoStream: Option[Subscription] = None
+    val generic: Receive = {
+      case Register(sub) =>
+        subscribers += sub
+        sub.s.onSubscribe(sub)
+      case Cancel(sub) =>
+        subscribers -= sub
+    }
+    private def makeHandler(r: Receive): Receive = 
+      r orElse generic
+    val stopped: Receive = makeHandler {
+      case video.Play =>
+        context become playing
+        startFile()
+    }
+    val paused: Receive = makeHandler {
+      case video.Play =>
+        // Request more frames, and continue
+        context become playing
+        requestMore(1)
+      case video.Stop => stop()
+      case f: video.Frame =>
+      // We should fire the frame on, but we don't need to request more.
+      // POSSIBLY we should just buffer this for later...
+        fireFrame(f)
+    }
+    val playing: Receive = makeHandler {
+      case video.Stop => stop()
+      case VideoSubscription(s) =>
+        videoStream = Some(s)
+        s.requestMore(1)
+      case video.Pause =>
+        context become paused
+      case f: video.Frame =>
+        fireFrame(f)
+      // TODO - Don't assume one subscriber...
+      case RequestMore(s, e) =>
+        requestMore(e)
+    }
+    override def receive = stopped
+    
+    private def requestMore(e: Int): Unit = 
+      videoStream.foreach(_.requestMore(e))
+    private def fireFrame(f: Frame): Unit =
+      for(s <- subscribers) {
+        s.s.onNext(f)
+      }
+    
+    private def stop(): Unit = {
+      context become stopped
+      videoStream foreach (_.cancel)
+      videoStream = None
+    }
+    
+    private def startFile(): Unit = {
+      object videoStreamSubscriber extends Subscriber[Frame] {
+        override def onSubscribe(subscription: Subscription): Unit = 
+          self ! VideoSubscription(subscription)
+        def onNext(element: Frame): Unit = self ! element
+        def onComplete(): Unit = self ! VideoComplete
+        def onError(cause: Throwable): Unit = self ! VideoError(cause)
+      }
+      video.FFMpeg.readFile(file).getPublisher.subscribe(videoStreamSubscriber)
+    }
+  }
+
+  
+  
+  // Gross details of creating a new SPI provider...
+  object getSubscriber extends Subscriber[UIControl] {
+    def onSubscribe(subscription: Subscription): Unit = () // TODO - what to do here
+    def onNext(element: UIControl): Unit = playerActor ! element
+    def onComplete(): Unit = () // TODO - Fire complete downstream
+    def onError(cause: Throwable): Unit = () /// TODO - fire downstream
+  }
+  object getPublisher extends Publisher[Frame] {
+    def subscribe(s: Subscriber[Frame]): Unit = {
+      playerActor ! Register(new Sub(s))
+    }
+  }
+  private case class Sub(val s: Subscriber[Frame]) extends Subscription {
+    override def requestMore(elements: Int): Unit = 
+      playerActor ! RequestMore(this, elements)
+    override def cancel(): Unit = playerActor ! Cancel(this)
+  }
+  private case class VideoSubscription(s: Subscription)
+  private case object VideoComplete
+  private case class VideoError(e: Throwable)
+  private case class Register(s: Sub)
+  private case class RequestMore(s: Sub, e: Int)
+  private case class Cancel(s: Sub)
+
+  private def openFile: Producer[Frame] = video.FFMpeg.readFile(file)
+}
--- src/main/scala/video/Display.scala
+++ src/main/scala/video/Display.scala
@@ -1,12 +1,14 @@
 package video
 
-
 import java.awt.image.BufferedImage
 import org.reactivestreams.spi.{
-  Publisher, Subscriber, Subscription
+  Publisher,
+  Subscriber,
+  Subscription
 }
 import org.reactivestreams.api.{
-  Producer, Consumer
+  Producer,
+  Consumer
 }
 import javax.swing.JComponent
 import java.util.concurrent.atomic.AtomicReference
@@ -15,10 +17,23 @@ import java.awt.Graphics
 import javax.swing.JFrame
 import java.awt.GridLayout
 import javax.swing.SwingUtilities
+import javax.swing.JPanel
+import java.awt.BorderLayout
+import javax.swing.JButton
+import java.awt.event.ActionListener
+import java.awt.event.ActionEvent
+import java.awt.Dimension
+import javax.swing.JLabel
+import java.awt.Component
 
+sealed trait UIControl
+case object Play extends UIControl
+case object Pause extends UIControl
+case object Stop extends UIControl
 
 /** A Swing component which will render frames to the screen as they are consumed. */
 class FrameDisplay extends JComponent with Consumer[Frame] {
+  setPreferredSize(new Dimension(640, 480))
   private var lastFrame: AtomicReference[Option[Frame]] = new AtomicReference(None)
   private var color = Color.BLACK
   private var frame = 0L
@@ -29,7 +44,7 @@ class FrameDisplay extends JComponent with Consumer[Frame] {
       subscription = s
       // Here we start asking for more frames.
       subscription.requestMore(1)
-      
+
     }
     def onNext(frame: Frame): Unit = {
       // TODO - This is to help relieve memory pressure, due to naive architecture.
@@ -48,14 +63,14 @@ class FrameDisplay extends JComponent with Consumer[Frame] {
       FrameDisplay.this.repaint()
     }
     // Errors and complete streams are treated the same, for now.
-    def onError(cause: Throwable ): Unit = onComplete()
+    def onError(cause: Throwable): Unit = onComplete()
   }
-  
+
   private def drawFrame(g: Graphics, frame: Frame): Unit =
-    if(g != null) {
+    if (g != null) {
       g.drawImage(frame.image, 0, 0, getWidth, getHeight, 0, 0, frame.image.getWidth, frame.image.getHeight, color, this)
     }
-    
+
   override protected def paintComponent(g: Graphics): Unit = {
     lastFrame.get match {
       case None =>
@@ -66,15 +81,99 @@ class FrameDisplay extends JComponent with Consumer[Frame] {
   }
 }
 
+/** Widget which wraps another component, and exposes play/pause/stop buttons. */
+class PlayerHolder(center: JComponent) extends JPanel with Producer[UIControl] {
+  private val buttonPanel = new JPanel()
+  private val playPauseButton = new JButton("play")
+  private val stopButton = new JButton("stop")
+  private var isPlaying = false
+  setLayout(new BorderLayout)
+  buttonPanel.setPreferredSize(new Dimension(640, 100))
+  buttonPanel.setMinimumSize(new Dimension(640, 100))
+  buttonPanel.setLayout(new GridLayout(1, 2))
+  playPauseButton.setMinimumSize(new Dimension(200, 100))
+  stopButton.setMinimumSize(new Dimension(200, 100))
+  buttonPanel.add(playPauseButton)
+  buttonPanel.add(stopButton)
+  add(center, BorderLayout.CENTER)
+  //add(new JLabel("Video Player"), BorderLayout.CENTER)
+  add(buttonPanel, BorderLayout.SOUTH)
+  setMinimumSize(new Dimension(640, 580))
+
+  playPauseButton.addActionListener(new ActionListener() {
+    override def actionPerformed(e: ActionEvent): Unit = {
+      if (isPlaying) {
+        playPauseButton.setLabel("play")
+        fireStreamEvent(Pause)
+      } else {
+        playPauseButton.setLabel("pause")
+        fireStreamEvent(Play)
+      }
+      isPlaying = !isPlaying
+    }
+  });
+  stopButton.addActionListener(new ActionListener() {
+    override def actionPerformed(e: ActionEvent): Unit = {
+      playPauseButton.setLabel("play")
+      isPlaying = false
+      fireStreamEvent(Stop)
+    }
+  })
+
+  object getPublisher extends Publisher[UIControl] {
+    def subscribe(subscriber: Subscriber[UIControl]): Unit = {
+      val sub = new Sub(subscriber)
+      subscriptions += sub
+      // TODO - add to list
+      subscriber onSubscribe sub
+    }
+  }
+  // TODO - real thread safety around this
+  @volatile
+  private var subscriptions: Set[Sub] = Set.empty
+
+  def produceTo(c: Consumer[UIControl]): Unit =
+    getPublisher subscribe c.getSubscriber
+
+  private case class Sub(c: Subscriber[UIControl]) extends Subscription {
+    override def cancel(): Unit = {
+      subscriptions -= this
+    }
+    override def requestMore(elements: Int): Unit = () // Ignore requests for more, we just always
+    // fire...
+
+    def fireStreamEvent(e: UIControl): Unit = {
+      c onNext e
+    }
+  }
+
+  // TODO - Implement.
+  private def fireStreamEvent(e: UIControl): Unit =
+    subscriptions.foreach(_ fireStreamEvent e)
+
+}
+
 object Display {
   def create(): Consumer[Frame] = {
-    val jframe = new JFrame("Display Window")
-    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     val display = new FrameDisplay()
-    jframe.add(display)
-    jframe.setLayout(new GridLayout(1,1))
-    jframe.setSize(640, 480)
-    jframe.setVisible(true)
+    inFrame("Video Preview", display)
     display
+  }
+  def createPlayer(): (Consumer[Frame], Producer[UIControl]) = {
+    val display = new FrameDisplay()
+    val controller = new PlayerHolder(display)
+    inFrame("Video Player", controller)
+    display -> controller
+  }
+
+  private def inFrame[T](title: String, c: Component): Unit = {
+    val jframe = new JFrame(title)
+    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    val pane = jframe.getContentPane
+    pane.setLayout(new GridLayout(1, 1))
+    pane.add(c)
+    jframe.setSize(640, 580)
+    //jframe.pack()	
+    jframe.setVisible(true)
   }
 }
